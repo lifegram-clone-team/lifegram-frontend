a9c14d504d88bfc1ce010e3309069e2de0e5c8db
Art-mj1 <mjmjpc@naver.com>
2023-07-25 01:07:51 +0900

    rename:Nav 파일 수정

--- src/routes/nav.js
+++ src/routes/nav.js
@@ -3,19 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Header from '../components/common/Header';
 import Profile from '../pages/Profile';
 import DetailModal from '../components/detail/DetailModal';
-import ModalAdd from '../components/add/ModalAdd';
-
-
 
 const Nav = () => {
-  //네비게이션
   return (
     <Routes>
       <Route path='/' element={<Header />}>
         <Route path='profile' element={<Profile />} />
       </Route>
       <Route path='/detail' element={<DetailModal />} />
-      <Route path='/add' element={<ModalAdd />} />
     </Routes>
   );
 };
