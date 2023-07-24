34a43119f68f36c9240733de4db8f9582be5ac7f
HyeonE <mag30102000@gmail.com>
2023-07-24 11:15:10 +0900

    update: 라우터 수정

--- src/routes/Nav.js
+++ src/routes/Nav.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Header from '../components/common/Header';
-import Profile from '../pages/Profile';
-import DetailModal from '../components/detail/DetailModal';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "../components/common/Header";
+import Profile from "../pages/Profile";
+import Main from "../pages/Main";
+import DetailModal from "../components/detail/DetailModal";
 
 const Nav = () => {
   return (
     <Routes>
-      <Route path='/' element={<Header />}>
-        <Route path='profile' element={<Profile />} />
+      <Route path="/" element={<Header />}>
+        <Route path="main" element={<Main />} />
+        <Route path="profile" element={<Profile />} />
       </Route>
-      <Route path='/detail' element={<DetailModal />} />
+      <Route path="/detail" element={<DetailModal />} />
     </Routes>
   );
 };
