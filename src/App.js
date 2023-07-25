15be2e14b321f772d187f871eb192a43756780a9
metablog <khu10735@naver.com>
2023-07-25 14:08:26 +0900

    update: 기존 코드를 react-query로 전환

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Nav from './routes/Nav';
-
+import { QueryClientProvider, QueryClient } from 'react-query';
+const queryClient = new QueryClient();
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Nav />
-    </>
+    </QueryClientProvider>
   );
 };
 
